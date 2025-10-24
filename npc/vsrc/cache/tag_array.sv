927ba905cd9bcfb19413be1c6eba5d5ac40a6531
Zaire404 <3147879462@qq.com>
2025-10-24 16:51:07 +0800

    feat 2R1W SRAM

--- npc/vsrc/cache/tag_array.sv
+++ npc/vsrc/cache/tag_array.sv
@@ -1,3 +1,4 @@
+// 2R1W Tag Array Module
 module tag_array #(
     parameter int unsigned NUM_WAYS = 4,
     parameter int unsigned NUM_BANKS = 4,
@@ -8,64 +9,84 @@ module tag_array #(
     input logic clk_i,
     input logic rst_ni,
 
-    // 读/写共用的地址端口 (来自CPU的Index)
-    input logic [SETS_PER_BANK_WIDTH-1:0] bank_addr_i,  // Bank内的地址 (Index的高位)
-    input logic [  $clog2(NUM_BANKS)-1:0] bank_sel_i,   // Bank选择 (Index的低位)
+    // --- 读端口 A (用于 Line 1) ---
+    input logic [SETS_PER_BANK_WIDTH-1:0] bank_addr_ra_i,  // Bank内读地址 A (Index)
+    input logic [$clog2(NUM_BANKS)-1:0] bank_sel_ra_i,  // Bank选择 A
+    output logic [NUM_WAYS-1:0][TAG_WIDTH-1:0] rdata_tag_a_o,  // 读出的Tag A
+    output logic [NUM_WAYS-1:0][VALID_WIDTH-1:0] rdata_valid_a_o,  // 读出的Valid A
 
-    // 写端口 (用于Cache Fill)
-    input logic [   NUM_WAYS-1:0] we_way_mask_i,  // 写使能掩码, 决定写哪一路
-    input logic [  TAG_WIDTH-1:0] wdata_tag_i,    // 要写入的Tag
-    input logic [VALID_WIDTH-1:0] wdata_valid_i,  // 要写入的Valid
+    // --- 读端口 B (用于 Line 2) ---
+    input logic [SETS_PER_BANK_WIDTH-1:0] bank_addr_rb_i,  // Bank内读地址 B (Index)
+    input logic [$clog2(NUM_BANKS)-1:0] bank_sel_rb_i,  // Bank选择 B
+    output logic [NUM_WAYS-1:0][TAG_WIDTH-1:0] rdata_tag_b_o,  // 读出的Tag B
+    output logic [NUM_WAYS-1:0][VALID_WIDTH-1:0] rdata_valid_b_o,  // 读出的Valid B
 
-    // 读端口 (用于Cache Lookup, 组合读)
-    output logic [NUM_WAYS-1:0][  TAG_WIDTH-1:0] rdata_tag_o,   // 读出的Tag (所有Way)
-    output logic [NUM_WAYS-1:0][VALID_WIDTH-1:0] rdata_valid_o  // 读出的Valid (所有Way)
+    // --- 写端口 (用于 Refill) ---
+    input logic [SETS_PER_BANK_WIDTH-1:0] w_bank_addr_i,   // Bank内写地址 (Index)
+    input logic [  $clog2(NUM_BANKS)-1:0] w_bank_sel_i,    // Bank选择 (写)
+    input logic [   NUM_WAYS-1:0] we_way_mask_i,   // 写使能掩码
+    input logic [TAG_WIDTH-1:0] wdata_tag_i,  // 要写入的Tag
+    input logic [VALID_WIDTH-1:0] wdata_valid_i  // 要写入的Valid
 );
 
-  // 每个SRAM存储的数据 = Tag + Valid
+  // 每个SRAM存储的数据 = Tag + Valids
   localparam int unsigned SRAM_DATA_WIDTH = TAG_WIDTH + VALID_WIDTH;
 
   // 内部连线
   logic [SRAM_DATA_WIDTH-1:0] sram_wdata;
-  logic [SRAM_DATA_WIDTH-1:0] sram_rdata [NUM_WAYS][NUM_BANKS];
-  logic                       sram_we    [NUM_WAYS][NUM_BANKS];
+  logic [SRAM_DATA_WIDTH-1:0] sram_rdata_a[NUM_WAYS][NUM_BANKS];
+  logic [SRAM_DATA_WIDTH-1:0] sram_rdata_b[NUM_WAYS][NUM_BANKS];
+  logic                       sram_we     [NUM_WAYS][NUM_BANKS];
 
   // 待写入的数据
   assign sram_wdata = {wdata_tag_i, wdata_valid_i};
 
-  // --- 实例化 NUM_WAYS * NUM_BANKS 个 SRAM ---
+  // --- 实例化 NUM_WAYS * NUM_BANKS 个 2R1W SRAM ---
   genvar i, j;
   generate
     for (i = 0; i < NUM_WAYS; i = i + 1) begin : gen_ways
       for (j = 0; j < NUM_BANKS; j = j + 1) begin : gen_banks
 
-        // 写使能逻辑：
-        // 当 (1) 这一路(Way)被选中 且 (2) 这一个Bank被选中时，才使能SRAM的写
-        assign sram_we[i][j] = we_way_mask_i[i] && (bank_sel_i == j);
+        // 写使能逻辑
+        assign sram_we[i][j] = we_way_mask_i[i] && (w_bank_sel_i == j);
 
         sram #(
             .DATA_WIDTH(SRAM_DATA_WIDTH),
             .ADDR_WIDTH(SETS_PER_BANK_WIDTH)
         ) tag_sram_inst (
-            .clk_i  (clk_i),
-            .rst_ni (rst_ni),
+            .clk_i (clk_i),
+            .rst_ni(rst_ni),
+
+            // 写端口
             .we_i   (sram_we[i][j]),
-            .addr_i (bank_addr_i),      // 所有的SRAM都使用相同的Bank内地址
-            .wdata_i(sram_wdata),       // 所有的SRAM都连接到相同的写数据
-            .rdata_o(sram_rdata[i][j])
+            .waddr_i(w_bank_addr_i),
+            .wdata_i(sram_wdata),
+
+            // 读端口 A
+            .addr_ra_i (bank_addr_ra_i),
+            .rdata_ra_o(sram_rdata_a[i][j]),
+
+            // 读端口 B
+            .addr_rb_i (bank_addr_rb_i),
+            .rdata_rb_o(sram_rdata_b[i][j])
         );
       end
     end
   endgenerate
 
-  // --- 读数据选择 (Mux) ---
-  // 读操作是组合的。`bank_addr_i`会使所有Bank的SRAM都读出数据。
-  // 我们需要根据 `bank_sel_i` 来选择正确的Bank的数据作为输出。
+  // --- 读数据选择 (Mux) A ---
   always_comb begin
     for (int i = 0; i < NUM_WAYS; i++) begin
-      // sram_rdata[i] 是一个 [NUM_BANKS-1:0] 的数组
-      // 我们只选择 `bank_sel_i` 对应的那个Bank的数据
-      {rdata_tag_o[i], rdata_valid_o[i]} = sram_rdata[i][bank_sel_i];
+      // 从 `bank_sel_ra_i` 对应的Bank选择数据
+      {rdata_tag_a_o[i], rdata_valid_a_o[i]} = sram_rdata_a[i][bank_sel_ra_i];
+    end
+  end
+
+  // --- 读数据选择 (Mux) B ---
+  always_comb begin
+    for (int i = 0; i < NUM_WAYS; i++) begin
+      // 从 `bank_sel_rb_i` 对应的Bank选择数据
+      {rdata_tag_b_o[i], rdata_valid_b_o[i]} = sram_rdata_b[i][bank_sel_rb_i];
     end
   end
 
--- npc/vsrc/test/tb_data_array.sv
+++ npc/vsrc/test/tb_data_array.sv
@@ -1,41 +1,54 @@
+// 文件: npc/vsrc/test/tb_data_array.sv (修改版)
 module tb_data_array #(
-    // --- Parameters for this test instance ---
     parameter int unsigned NUM_WAYS_TEST = 4,
     parameter int unsigned NUM_BANKS_TEST = 4,
     parameter int unsigned SETS_PER_BANK_WIDTH_TEST = 8,
-    parameter int unsigned BLOCK_WIDTH_TEST = 512  // 假设 64 字节块 = 512 位
+    parameter int unsigned BLOCK_WIDTH_TEST = 512
 ) (
-    // --- Top-level ports for C++ access ---
     input logic clk_i,
     input logic rst_ni,
 
-    // Address ports
-    input logic [SETS_PER_BANK_WIDTH_TEST-1:0] bank_addr_i,
-    input logic [  $clog2(NUM_BANKS_TEST)-1:0] bank_sel_i,
+    // --- 读端口 A (用于 Line 1) ---
+    input logic [SETS_PER_BANK_WIDTH_TEST-1:0] bank_addr_ra_i,
+    input logic [$clog2(NUM_BANKS_TEST)-1:0] bank_sel_ra_i,
+    output logic [NUM_WAYS_TEST-1:0][BLOCK_WIDTH_TEST-1:0] rdata_a_o,
 
-    // Write ports
+    // --- 读端口 B (用于 Line 2) ---
+    input logic [SETS_PER_BANK_WIDTH_TEST-1:0] bank_addr_rb_i,
+    input logic [$clog2(NUM_BANKS_TEST)-1:0] bank_sel_rb_i,
+    output logic [NUM_WAYS_TEST-1:0][BLOCK_WIDTH_TEST-1:0] rdata_b_o,
+
+    // --- 写端口 ---
+    input logic [SETS_PER_BANK_WIDTH_TEST-1:0] w_bank_addr_i,
+    input logic [  $clog2(NUM_BANKS_TEST)-1:0] w_bank_sel_i,
     input logic [   NUM_WAYS_TEST-1:0] we_way_mask_i,
-    input logic [BLOCK_WIDTH_TEST-1:0] wdata_i,
-
-    // Read ports
-    output logic [NUM_WAYS_TEST-1:0][BLOCK_WIDTH_TEST-1:0] rdata_o
+    input logic [BLOCK_WIDTH_TEST-1:0] wdata_i
 );
 
-  // --- Instantiate the Device Under Test (DUT) ---
+  // --- 实例化 DUT ---
   data_array #(
       .NUM_WAYS(NUM_WAYS_TEST),
       .NUM_BANKS(NUM_BANKS_TEST),
       .SETS_PER_BANK_WIDTH(SETS_PER_BANK_WIDTH_TEST),
       .BLOCK_WIDTH(BLOCK_WIDTH_TEST)
   ) DUT (
-      // Connect DUT ports directly to the testbench ports
-      .clk_i(clk_i),
+      .clk_i (clk_i),
       .rst_ni(rst_ni),
-      .bank_addr_i(bank_addr_i),
-      .bank_sel_i(bank_sel_i),
-      .we_way_mask_i(we_way_mask_i),
-      .wdata_i(wdata_i),
-      .rdata_o(rdata_o)
-  );
 
+      // 读端口 A
+      .bank_addr_ra_i(bank_addr_ra_i),
+      .bank_sel_ra_i(bank_sel_ra_i),
+      .rdata_a_o(rdata_a_o),
+
+      // 读端口 B
+      .bank_addr_rb_i(bank_addr_rb_i),
+      .bank_sel_rb_i(bank_sel_rb_i),
+      .rdata_b_o(rdata_b_o),
+
+      // 写端口
+      .w_bank_addr_i(w_bank_addr_i),
+      .w_bank_sel_i(w_bank_sel_i),
+      .we_way_mask_i(we_way_mask_i),
+      .wdata_i(wdata_i)
+  );
 endmodule
