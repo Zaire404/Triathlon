043ef4cfb5a781285f112bc1142fb539c2e37b98
Zaire404 <3147879462@qq.com>
2025-10-31 13:02:53 +0800

    feat: icache for lfu

--- npc/vsrc/include/test_config_pkg.sv
+++ npc/vsrc/include/test_config_pkg.sv
@@ -5,11 +5,11 @@ package test_config_pkg;
   localparam config_pkg::user_cfg_t TestCfg = '{
       INSTR_PER_FETCH : unsigned'(4),
       XLEN          : unsigned'(32),
-      VLEN          : unsigned'(64),
+      VLEN          : unsigned'(32),
       ILEN          : unsigned'(32),
       ICACHE_BYTE_SIZE : unsigned'(4096),
       ICACHE_SET_ASSOC : unsigned'(4),
-      ICACHE_LINE_WIDTH : unsigned'(64)
+      ICACHE_LINE_WIDTH : unsigned'(256)
   };
 
 endpackage : test_config_pkg
