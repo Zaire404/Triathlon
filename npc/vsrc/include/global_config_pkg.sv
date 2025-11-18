1d9822363dae3b8348dec16e01619bf8093cf79b
sy <2385753727@qq.com>
2025-11-18 21:02:45 +0800

    重新写了ifu模块，现在的ifu中有一个pc寄存器，有一个3状态的状态机操控数据流

--- npc/vsrc/include/global_config_pkg.sv
+++ npc/vsrc/include/global_config_pkg.sv
@@ -7,18 +7,11 @@ package global_config_pkg;
 
   localparam config_pkg::cfg_t Cfg = build_config_pkg::build_config(test_config_pkg::TestCfg);
 
-
-
-  // IFU <-> ICache
-  typedef struct packed {
-    logic                                         valid;  // request is valid
-    logic [Cfg.INSTR_PER_FETCH-1:0][Cfg.VLEN-1:0] vaddr;  // virtual address to fetch
-  } ifu2icache_req_t;
-
   typedef struct packed {
     logic                                         ready;
-    logic [Cfg.INSTR_PER_FETCH-1:0][Cfg.ILEN-1:0] data;
-  } icache2ifu_rsp_t;
+    logic                                         valid;  // request is valid
+  } handshake_t;
+  
 
   typedef struct packed {
     logic valid;
