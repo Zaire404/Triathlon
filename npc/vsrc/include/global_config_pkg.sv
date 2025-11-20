c4ac8c082c9d3cdf097fe223d108030d6471b2cb
sy <2385753727@qq.com>
2025-11-20 14:22:17 +0800

    1

--- npc/vsrc/include/global_config_pkg.sv
+++ npc/vsrc/include/global_config_pkg.sv
@@ -8,15 +8,6 @@ package global_config_pkg;
 
   localparam config_pkg::cfg_t Cfg = build_config_pkg::build_config(test_config_pkg::TestCfg);
 
-<<<<<<< HEAD
-  typedef struct packed {
-    logic                                         ready;
-    logic                                         valid;  // request is valid
-  } handshake_t;
-  
-
-=======
->>>>>>> main
   typedef struct packed {
     logic valid;
     logic ready;
