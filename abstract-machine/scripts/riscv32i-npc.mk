33cac7817a3eff274242b011420c67bbd0de4271
Zaire404 <3147879462@qq.com>
2026-02-27 18:51:18 +0800

    arch: split rv32e and rv32i AM npc toolchain profiles

--- abstract-machine/scripts/riscv32i-npc.mk
+++ abstract-machine/scripts/riscv32i-npc.mk
@@ -1,8 +1,8 @@
 include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/npc.mk
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
-COMMON_CFLAGS += -march=rv32em_zicsr -mabi=ilp32e  # overwrite
-LDFLAGS       += -melf32lriscv                      # overwrite
+COMMON_CFLAGS += -march=rv32im_zicsr_zifencei -mabi=ilp32  # overwrite
+LDFLAGS       += -melf32lriscv                              # overwrite
 
 AM_SRCS += riscv/npc/libgcc/div.S \
            riscv/npc/libgcc/muldi3.S \
